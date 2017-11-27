167ea3b8b9e97f0e1103014a62bfd3fd74e40f74
csgaroutte <chrisgaroutte@outlook.com>
2017-11-27 15:14:35 -0800

    C++ Implementation of NN

---
+++ nearestNeighbor.hpp
@@ -0,0 +1,9 @@
+#ifndef NEAREST_NEIGHBOR_HPP
+#define NEAREST_NEIGHBOR_HPP
+
+#include <vector>
+#include <cmath>
+
+void nearestNeighbor(std::vector<std::vector<int>*> C, std::vector<double>& T, int startPoint);
+
+#endif
