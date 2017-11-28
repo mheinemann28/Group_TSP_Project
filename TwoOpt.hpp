ef8b746ae74814e58204ac65040d54d28422dff8
csgaroutte <chrisgaroutte@outlook.com>
2017-11-28 13:40:53 -0800

    Delete TwoOpt.hpp

--- TwoOpt.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TWO_OPT_HPP
-#define TWO_OPT_HPP
-
-#include <vector>
-
-void TwoOpt(const std::vector< std::vector<int>* >& C, std::vector<double>& T);
-void TwoOptSwap(const std::vector<double> tour, std::vector<double>& newRoute, int i, int k);
-
-#endif
