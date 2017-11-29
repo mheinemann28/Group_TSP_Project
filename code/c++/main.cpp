717d5b5915776a3128f3b4bf0063b59d7ff9b1e0
csgaroutte <chrisgaroutte@outlook.com>
2017-11-28 18:23:20 -0800

    Added command line options for optimized/ fast

--- code/c++/main.cpp
+++ code/c++/main.cpp
@@ -7,45 +7,59 @@
 
 int main(int argc, char* argv[])
 {
+	if (argv[1])
+	{
+		// List to hold city data
+		std::vector< std::vector<int> * > Cities;
+
+		// Open file specified through command line
+		std::ifstream file(argv[1]);
+
+		// Check if file exists, exit if it does not
+		if (!file) {
+			std::cerr << "File could not be read for opening. Exiting program." << std::endl;
+			std::exit(EXIT_FAILURE);
+		}
+
+		// Read data from file into separate vectors, ppending each one to Cities vector
+		int city, x, y;
+		while (file >> city)
+		{
+			file >> x >> y;
+			std::vector<int>* tempArray = new std::vector<int>;
+			tempArray->push_back(city);
+			tempArray->push_back(x);
+			tempArray->push_back(y);
+
+			Cities.push_back(tempArray);
+		}
+
+		std::vector<double> Tour;
+
+		// Run nearest neighbor approximation algorithm
+		nearestNeighbor(Cities, Tour, Cities.at(0)->at(0));
 	
-	// List to hold city data
-	std::vector< std::vector<int> * > Cities;
+		// Run two-opt if optimizaton option chosen
+		if (argv[2] && *argv[2] == 'o')
+		{
+			TwoOpt(Cities, Tour);
+		}
 
-	// Open file specified through command line
-	std::ifstream file(argv[1]);
 
-	// Check if file exists, exit if it does not
-	if (!file) {
-		std::cerr << "File could not be read for opening. Exiting program." << std::endl;
-		exit(1);
+		// Write results to file
+		std::string outfileName = std::string(argv[1]);
+		outfileName.append(".tour");
+		std::ofstream outfile(outfileName);
+
+		for (int i = 0; i < Tour.size(); i++)
+		{
+			outfile << Tour.at(i) << '\n';
+		}
 	}
-
-	// Read data from file into separate vectors, ppending each one to Cities vector
-	int city, x, y;
-	while (file >> city)
+	else
 	{
-		file >> x >> y;
-		std::vector<int>* tempArray = new std::vector<int>;
-		tempArray->push_back(city);
-		tempArray->push_back(x);
-		tempArray->push_back(y);
-
-		Cities.push_back(tempArray);
-	}
-
-	std::vector<double> Tour;
-
-	nearestNeighbor(Cities, Tour, Cities.at(0)->at(0));
-	TwoOpt(Cities, Tour);
-
-	// Write results to file
-	std::string outfileName = std::string(argv[1]);
-	outfileName.append(".tour");
-	std::ofstream outfile(outfileName);
-
-	for (int i = 0; i < Tour.size(); i++)
-	{
-		outfile << Tour.at(i) << '\n';
+		std::cerr << "Missing command line arguments. Please re-run with the correct number/type of arguments.";
+		std::exit(EXIT_FAILURE);
 	}
 	return 0;
 }
