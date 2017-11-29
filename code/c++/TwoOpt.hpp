33901ba2d8c1d8497d384774e71b747e94d5edfb
csgaroutte <chrisgaroutte@outlook.com>
2017-11-28 17:27:54 -0800

    Added <cmath> include

--- code/c++/TwoOpt.hpp
+++ code/c++/TwoOpt.hpp
@@ -2,6 +2,7 @@
 #define TWO_OPT_HPP
 
 #include <vector>
+#include <cmath>
 
 void TwoOpt(const std::vector< std::vector<int>* >& C, std::vector<double>& T);
 void TwoOptSwap(const std::vector<double> tour, std::vector<double>& newRoute, int i, int k);
